cf29cad852812aefa15bbbd65eee124ad80e4563
ThiagoMoraes <thiago.pvni1@gmail.com>
2016-03-31 10:50:24 -0300

    BubbleSortExercise

---
+++ BubblesortExercise/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BubblesortExercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bubblesort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bubblesort
+            // 
+            this.bubblesort.Location = new System.Drawing.Point(12, 12);
+            this.bubblesort.Name = "bubblesort";
+            this.bubblesort.Size = new System.Drawing.Size(100, 23);
+            this.bubblesort.TabIndex = 0;
+            this.bubblesort.Text = "Bubblesort";
+            this.bubblesort.UseVisualStyleBackColor = true;
+            this.bubblesort.Click += new System.EventHandler(this.BubbleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Antes : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Depois : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 449);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bubblesort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bubblesort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BubblesortExercise/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BubblesortExercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> lista = new List<int>();
+        int comparisons = 0;
+        int changes = 0;
+        int aux = 0;
+        
+
+        private void BubbleClick(object sender, EventArgs e)
+        {
+         Stopwatch time = new Stopwatch();
+            time.Start();
+            
+                    for (int write = 0; write < name.Length; write++)
+                    {
+                        for (int sort = 0; sort < name.Length - 1; sort++)
+                        {
+                            comparisons++;
+                            if (name[sort] > name[sort + 1])
+                            {
+                                aux = name[sort + 1];
+                                name[sort + 1] = name[sort];
+                                name[sort] = aux;
+                                changes++;
+                            }
+                        }
+                    }
+            
+            time.Stop();
+         
+        }
+    }
+}
